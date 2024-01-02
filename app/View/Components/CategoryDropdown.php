cc4039af82e8811ac50f928ae46ff0993076323b
Michael Schennum <mike@aware3.com>
2024-01-02 08:16:00 -0700

    add in css and design

--- app/View/Components/CategoryDropdown.php
+++ app/View/Components/CategoryDropdown.php
@@ -5,25 +5,18 @@ namespace App\View\Components;
 use Closure;
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
+use App\Models\Category;
 
 class CategoryDropdown extends Component
 {
-    /**
-     * Create a new component instance.
-     */
-    public function __construct()
-    {
-        //
-    }
-
     /**
      * Get the view / contents that represent the component.
      */
     public function render(): View|Closure|string
     {
         return view('components.category-dropdown', [
-			'categories' => \App\Models\Category::all(),
-			'currentCategory' => \App\Models\Category::firstWhere('slug', request('category')),
+			'categories' => Category::all(),
+			'currentCategory' => Category::firstWhere('slug', request('category')),
 		]);
     }
 }
