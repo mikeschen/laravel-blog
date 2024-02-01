e3f80832906c53b7ab2ca5d884719595c5fbb352
Michael Schennum <mike@aware3.com>
2024-02-01 08:48:46 -0700

    add in Mailchimp

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,24 @@ use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
 use App\Models\Category;
 use App\Models\User;
+use MailchimpMarketing\ApiClient;
+
+Route::get('ping', function () {
+
+	$mailchimp = new \MailchimpMarketing\ApiClient();
+
+	$mailchimp->setConfig([
+		'apiKey' => config('services.mailchimp.key'),
+		'server' => 'us21'
+	]);
+
+	$response = $mailchimp->lists->addListMember('942a7902f6', [
+		'email_address' => 'schenstock@yahoo.com',
+		'status' => 'subscribed',
+	]);
+
+	ddd($response);
+});
 
 Route::get('/', [PostController::class, 'index'])->name('home');
 
